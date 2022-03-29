ca9135d8f1500aa036be603e8474b8cf00f795fd
Muhammad Hassan <hassankhan5805@gmail.com>
2022-03-29 08:53:23 +0500

    draggable + add ,remove item

--- lib/main.dart
+++ lib/main.dart
@@ -31,138 +31,134 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       List<int>.generate(50, (int index) => index);
   final List<int> _secondListItems =
       List<int>.generate(50, (int index) => index);
+  // late List<Widget> widgetList = [
+  //   reOrderList(_firstListItems, "0"),
+  //   reOrderList(_secondListItems, "1"),
+  // ];
+  late final List<List<int>> _lists = [_firstListItems, _secondListItems];
+  final List<String> keys = ["0", "1"];
+  func(int oldIndex, int newIndex, List<int> listItems) {
+    setState(() {
+      if (oldIndex < newIndex) {
+        newIndex -= 1;
+      }
+      final int item = listItems.removeAt(oldIndex);
+      listItems.insert(newIndex, item);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
-    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
-
-    return Column(
+    return ListView(
       children: [
         SizedBox(
           height: 550,
           child: ReorderableListView(
             padding: const EdgeInsets.symmetric(horizontal: 40),
             onReorder: (int oldIndex, int newIndex) {
-              // TODO: Coding instruction
+              setState(() {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final List<int> item = _lists.removeAt(oldIndex);
+                _lists.insert(newIndex, item);
+              });
+              // ODO: Coding instruction
               //       Do all code in this page
               //       Do not use other plugins
               //       Code need to be fully customizable
-              // TODO: Make the parent list also reordered
-              // TODO: Make lists item draggable and reordered between the lists (vertical & horizontal)
-              // TODO: Add an item to a particular list without rebuilding the other lists
-              // TODO: Remove an item to a particular list without rebuilding the other lists
-              // TODO: Make it responsive
+              // ODO: Make the parent list also reordered
+              // ODO: Make lists item draggable and reordered between the lists (vertical & horizontal)
+              // ODO: Add an item to a particular list without rebuilding the other lists
+              // ODO: Remove an item to a particular list without rebuilding the other lists
+              // ODO: Make it responsive
             },
             children: <Widget>[
-              SizedBox(
-                key: const Key('0'),
-                height: 250,
-                child: Column(
-                  children: [
-                    Container(
-                      height: 20,
-                      color: Colors.red,
-                    ),
-                    SizedBox(
-                      height: 200,
-                      child: ReorderableListView(
-                        scrollDirection: Axis.horizontal,
-                        padding: const EdgeInsets.symmetric(vertical: 40),
-                        children: <Widget>[
-                          for (int index = 0;
-                              index < _firstListItems.length;
-                              index += 1)
-                            SizedBox(
-                              width: 150,
-                              key: Key('$index'),
-                              child: ListTile(
-                                tileColor: _firstListItems[index].isOdd
-                                    ? oddItemColor
-                                    : evenItemColor,
-                                title: Text('Item ${_firstListItems[index]}'),
-                              ),
-                            ),
-                        ],
-                        onReorder: (int oldIndex, int newIndex) {
-                          setState(() {
-                            if (oldIndex < newIndex) {
-                              newIndex -= 1;
-                            }
-                            final int item = _firstListItems.removeAt(oldIndex);
-                            _firstListItems.insert(newIndex, item);
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                key: const Key('1'),
-                height: 250,
-                child: Column(
-                  children: [
-                    Container(
-                      height: 20,
-                      color: Colors.red,
-                    ),
-                    SizedBox(
-                      height: 200,
-                      child: ReorderableListView(
-                        scrollDirection: Axis.horizontal,
-                        padding: const EdgeInsets.symmetric(vertical: 40),
-                        children: <Widget>[
-                          for (int index = 0;
-                              index < _secondListItems.length;
-                              index += 1)
-                            SizedBox(
-                              width: 150,
-                              key: Key('$index'),
-                              child: ListTile(
-                                tileColor: _secondListItems[index].isOdd
-                                    ? oddItemColor
-                                    : evenItemColor,
-                                title: Text('Item ${_secondListItems[index]}'),
-                              ),
-                            ),
-                        ],
-                        onReorder: (int oldIndex, int newIndex) {
-                          setState(() {
-                            if (oldIndex < newIndex) {
-                              newIndex -= 1;
-                            }
-                            final int item =
-                                _secondListItems.removeAt(oldIndex);
-                            _secondListItems.insert(newIndex, item);
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              for (int index = 0; index < _lists.length; index += 1)
+                reOrderList(_lists[index], "$index")
             ],
           ),
         ),
         Row(
           children: [
             TextButton(
-                onPressed: () {}, child: const Text('Add item to list 1')),
+                onPressed: () {
+                  setState(() {
+                    _firstListItems.insert(
+                        _firstListItems.length , _firstListItems.length);
+                  });
+                },
+                child: const Text('Add item to list 1')),
             TextButton(
-                onPressed: () {}, child: const Text('Add item to list 2')),
+                onPressed: () {
+                  setState(() {
+                    _secondListItems.insert(
+                        _secondListItems.length, _secondListItems.length);
+                  });
+                },
+                child: const Text('Add item to list 2')),
           ],
         ),
         Row(
           children: [
             TextButton(
-                onPressed: () {}, child: const Text('Remove item from list 1')),
+                onPressed: () {
+                  setState(() {
+                    _firstListItems.removeAt(_firstListItems.length - 1);
+                  });
+                },
+                child: const Text('Remove item from list 1')),
             TextButton(
-                onPressed: () {}, child: const Text('Remove item from list 2')),
+                onPressed: () {
+                  setState(() {
+                    _secondListItems.removeAt(_secondListItems.length - 1);
+                  });
+                },
+                child: const Text('Remove item from list 2')),
           ],
         ),
       ],
+    );
+  }
+
+  Widget reOrderList(final List<int> listItems, String keys) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+    return SizedBox(
+      key: Key(keys),
+      height: 250,
+      child: Column(
+        children: [
+          Container(
+            height: 20,
+            color: Colors.red,
+          ),
+          SizedBox(
+            height: 200,
+            child: ReorderableListView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.symmetric(vertical: 40),
+              children: <Widget>[
+                for (int index = 0; index < listItems.length; index += 1)
+                  SizedBox(
+                    width: 150,
+                    key: Key('$index'),
+                    child: ListTile(
+                      tileColor: _secondListItems[index].isOdd
+                          ? oddItemColor
+                          : evenItemColor,
+                      title: Text('Item ${listItems[index]}'),
+                    ),
+                  ),
+              ],
+              onReorder: (int oldIndex, int newIndex) {
+                func(oldIndex, newIndex, listItems);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
